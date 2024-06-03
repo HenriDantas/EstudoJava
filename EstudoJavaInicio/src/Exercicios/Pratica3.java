28eb42e0a89bc3973a55da33f16197de29aadd52
Henri Dantas <santoshenri0@gmail.com>
2024-06-03 11:45:11 -0300

    fim pratica 3

---
+++ EstudoJavaInicio/src/Exercicios/Pratica3.java
@@ -0,0 +1,106 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Pratica3 {
+    public static void main(String[] args) {
+        
+        // Scanner ler = new Scanner(System.in);
+        // System.out.println("Escreva um numero qualquer");
+        // double num = ler.nextDouble();
+        // if (num > 0) {
+        //     System.out.println("Numero positivo");
+        // } else {
+        //     System.out.println("Numero negativo");
+        // } 
+        
+//==============================================================
+
+        // Scanner ler = new Scanner(System.in);
+        // System.out.println("Escreva o numero 1: ");
+        // int n1 = ler.nextInt();
+        // System.out.println("Escreva o numero 2: ");
+        // int n2 = ler.nextInt();
+        // if (n1 == n2){
+        //     System.out.println("iguais");
+        // } else {
+        //     System.out.println("diferentes");
+        //     if (n1 > n2) {
+        //         System.out.println("1 é maior que o 2");
+        //     } else {
+        //         System.out.println("2 é maior que 1");
+        //     }
+        // }
+
+//==============================================================
+
+        // Scanner ler = new Scanner(System.in);
+        // System.out.println("==================================");
+        // System.out.println("       Escolha a opção");
+        // System.out.println("  1 - calcular area do quadrado");
+        // System.out.println("  2 - calcular area do circulo");
+        // System.out.println("==================================");
+        // int opcao = ler.nextInt();
+        // // if ( opcao == 1){
+        // //     double base = 2;
+        // //     double altura = 3;
+        // //     double area = base * altura;
+        // //     System.out.println("area quadrado: "+ area);
+        // // } else {
+        // //     double pi = 3;
+        // //     double raio = 5;
+        // //     double area = pi * (raio * raio);
+        // //     System.out.println("Area circulo: "+ area);
+        // // }
+        // switch (opcao) {
+        //     case 1:
+        //         double base = 2;
+        //         double altura = 3;
+        //         double areaQ = base * altura;
+        //         System.out.println("area quadrado: "+ areaQ);
+        //         break;
+        //     case 2:
+        //         double pi = 3;
+        //         double raio = 5;
+        //         double areaC = pi * (raio * raio);
+        //         System.out.println("Area circulo: "+ areaC);
+        //         break;
+        //     default:
+        //         System.out.println("erro, numero invalido");
+        //         break;
+        // }
+
+//==============================================================
+
+        // Scanner ler = new Scanner(System.in);
+        // System.out.println("Escolha um numero de 1 a 10");
+        // int num = ler.nextInt();
+        // for(int i = 0; i < 11; i++ ){
+        //     System.out.println("%d X %d = %d".formatted(num, i, num * i));
+        // }
+
+//==============================================================
+
+        // Scanner ler = new Scanner(System.in);
+        // System.out.println("Digite um numero inteiro");
+        // int num = ler.nextInt();
+        // if (num % 2 == 0) {
+        //     System.out.println("ele é par");
+        // } else {
+        //     System.out.println("ele é impar");
+        // }
+
+//==============================================================
+
+        // Scanner ler = new Scanner(System.in);
+        // System.out.println("Digite um numero");
+        // double num = ler.nextDouble();
+        // double fatorial = num;
+        // for (int i = 1; i < num; i++){
+        //     System.out.println(fatorial * i);
+        //     // fatorial *= i;
+        // }
+        // // System.out.println(fatorial);
+        
+    }
+}
